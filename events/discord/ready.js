157064176d13c578fa0a55bf5dde3582ab29e7cf
Warren D. Rezane <warzkie123@gmail.com>
2023-06-02 13:42:38 +0800

    Updated the commands.

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -4,18 +4,18 @@ const { ActivityType } = require("discord.js");
 module.exports = async (client) => {
   await client.application?.commands.set([]);
 
-  // await register(
-  //   client,
-  //   client.register_arr.map((command) => ({
-  //     name: command.name,
-  //     description: command.description,
-  //     options: command.options,
-  //     type: "1",
-  //   })),
-  //   {
-  //     debug: true,
-  //   }
-  // );
+  await register(
+    client,
+    client.register_arr.map((command) => ({
+      name: command.name,
+      description: command.description,
+      options: command.options,
+      type: "1",
+    })),
+    {
+      debug: true,
+    }
+  );
   // // Register slash commands - ( If you are one of those people who read the codes I highly suggest ignoring this because I am very bad at what I am doing, thanks LMAO )
   console.log(`[ / | Slash Command ] - ✅ Loaded all slash commands!`);
   let invite = `https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot`;
